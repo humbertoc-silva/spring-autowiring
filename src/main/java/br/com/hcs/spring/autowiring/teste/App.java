08dbd495a03a323d57efce4930acae2e9199faf0
Humberto Corrêa <humbertoc_silva@hotmail.com>
2016-08-29 13:22:21 -0300

    Atualização da classe de teste
    Instanciação do bean Processador para testar a injeção de dependências.

--- src/main/java/br/com/hcs/spring/autowiring/teste/App.java
+++ src/main/java/br/com/hcs/spring/autowiring/teste/App.java
@@ -3,8 +3,11 @@ package br.com.hcs.spring.autowiring.teste;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import br.com.hcs.spring.autowiring.Processador;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Processador processador = context.getBean(Processador.class);
     }
 }
